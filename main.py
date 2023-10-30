58ebe2fc2dc9e9d96ffea80221996e95310b6525
Julianedias <149388662+Julianedias@users.noreply.github.com>
2023-10-30 14:48:51 +0000

    Terminei de copiar o código

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ if(idade>=18):
     print("Pode ter o desconto")
     desconto = total*0.1
     final = total - desconto
-    print(f"O desconto foi de R${desconto: 2.f} e o total é de  R${final}")
+    print(f"O desconto foi de R${desconto} e o total é de  R${final}")
 else:
     print(f"Não tem direito ao desconto e o seu valor final é de R${total}")
